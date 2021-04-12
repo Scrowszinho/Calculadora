eb612245182900f0cd3b39114f1a41ccd188d323
Luis Gustavo <gustascrows@gmail.com>
2021-04-12 17:53:50 -0300

    Feat: Calculadora incial

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './Calcuator.css';
+
+export default class Calculator extends Component{
+    render(){
+        return(
+            <div>
+            <h1>Calculator</h1>
+            <div className='calculator'>
+
+            </div>
+            </div>
+        );
+    }
+}
