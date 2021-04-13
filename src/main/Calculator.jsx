dea43b6002767cb12cac4ec8e71a48af0bf08985
Luis Gustavo <gustascrows@gmail.com>
2021-04-13 16:32:23 -0300

    Feat: Setando no display, pegando primeiro valor

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -3,18 +3,43 @@ import './Calcuator.css';
 
 import Button from '../components/Button';
 import Display from '../components/Display';
+
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0
+}
+
 export default class Calculator extends Component {
 
-    clearMemory(){
-        console.log('Limpar');
+    state = { ...initialState }
+
+    clearMemory() {
+        this.setState({ ...initialState });
     }
 
-    setOperation(operation){
+    setOperation(operation) {
         console.log(operation);
     }
 
-    addDigit(n){
-        console.log(n);
+    addDigit(n) {
+        if (n === '.' && this.state.displayValue.includes('.')) {
+            return;
+        }
+        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay;
+        const currentValue = clearDisplay ? '' : this.state.displayValue;
+        const displayValue = currentValue + n;
+        this.setState({ displayValue, clearDisplay: false });
+        if (n !== '.') {
+            const i = this.state.current;
+            const newValue = parseFloat(displayValue);
+            const values = [...this.state.values];
+            values[i] = newValue;
+            this.setState({ values })
+            console.log(values);
+        }
     }
 
     render() {
@@ -24,24 +49,24 @@ export default class Calculator extends Component {
             <div>
                 <h1>Calculator</h1>
                 <div className='calculator'>
-                    <Display value={100}/>
-                    <Button label='AC' click={() => this.clearMemory()} triple/>
-                    <Button label='÷' click={setOperation} operation/>
-                    <Button label='7' click={addDigit}/>
-                    <Button label='8' click={addDigit}/>
-                    <Button label='9' click={addDigit}/>
-                    <Button label='*' click={setOperation} operation/>
-                    <Button label='4' click={addDigit}/>
-                    <Button label='5' click={addDigit}/>
-                    <Button label='6' click={addDigit}/>
-                    <Button label='-' click={setOperation} operation/>
-                    <Button label='1' click={addDigit}/>
-                    <Button label='2' click={addDigit}/>
-                    <Button label='3' click={addDigit}/>
-                    <Button label='+' click={setOperation} operation/>
-                    <Button label='0' click={addDigit} double/>
-                    <Button label='.' click={addDigit}/>
-                    <Button label='=' click={setOperation} operation/>
+                    <Display value={this.state.displayValue} />
+                    <Button label='AC' click={() => this.clearMemory()} triple />
+                    <Button label='÷' click={setOperation} operation />
+                    <Button label='7' click={addDigit} />
+                    <Button label='8' click={addDigit} />
+                    <Button label='9' click={addDigit} />
+                    <Button label='*' click={setOperation} operation />
+                    <Button label='4' click={addDigit} />
+                    <Button label='5' click={addDigit} />
+                    <Button label='6' click={addDigit} />
+                    <Button label='-' click={setOperation} operation />
+                    <Button label='1' click={addDigit} />
+                    <Button label='2' click={addDigit} />
+                    <Button label='3' click={addDigit} />
+                    <Button label='+' click={setOperation} operation />
+                    <Button label='0' click={addDigit} double />
+                    <Button label='.' click={addDigit} />
+                    <Button label='=' click={setOperation} operation />
                 </div>
             </div>
         );
