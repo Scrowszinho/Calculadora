62c81ccff97bcd81c59120fe99977d0369c06561
Luis Gustavo <gustascrows@gmail.com>
2021-04-13 15:30:19 -0300

    Feat: Componente calculator att

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,14 +1,30 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './Calcuator.css';
-
-export default class Calculator extends Component{
-    render(){
-        return(
+import Button from '../components/Button';
+export default class Calculator extends Component {
+    render() {
+        return (
             <div>
-            <h1>Calculator</h1>
-            <div className='calculator'>
-
-            </div>
+                <h1>Calculator</h1>
+                <div className='calculator'>
+                    <Button label='AC'/>
+                    <Button label='÷'/>
+                    <Button label='7'/>
+                    <Button label='8'/>
+                    <Button label='9'/>
+                    <Button label='*'/>
+                    <Button label='4'/>
+                    <Button label='5'/>
+                    <Button label='6'/>
+                    <Button label='-'/>
+                    <Button label='1'/>
+                    <Button label='2'/>
+                    <Button label='3'/>
+                    <Button label='+'/>
+                    <Button label='0'/>
+                    <Button label='.'/>
+                    <Button label='='/>
+                </div>
             </div>
         );
     }
